58d0fd6d1863b74e05610dccc6ae274b37310409
Dasha6elka <CB4kot@gmail.com>
2021-01-21 09:56:40 +0300

    Fix: ast tree parsing

--- src/SLR1/ast.v2.ts
+++ src/SLR1/ast.v2.ts
@@ -1,6 +1,6 @@
 import graphviz, { Node as GrapNode } from "graphviz";
 import { nanoid } from "nanoid";
-import { escapeRegExp, map } from "lodash";
+import { map } from "lodash";
 
 import acorn = require("acorn");
 
@@ -10,20 +10,12 @@ type AcornNode = Record<string, any> & acorn.Node;
 type TreeFunctions = Record<string, (node: AcornNode, _: unknown, c: (node: AcornNode, _: unknown) => void) => void>;
 
 export function createAst(text: string) {
-    let source = text
+    const source = text
         .replace(/int .+;/gm, "")
         .replace(/double .+;/gm, "")
         .replace(/boolean .+;/gm, "")
         .replace(/string .+;/gm, "")
         .replace(/;/g, " ");
-
-    // let match: RegExpMatchArray | null = null;
-    // while ((match = source.match(/(?<=\s|\()-(([0-9]\.?([0-9]+)?)|[a-z]|\(.*\))/))) {
-    //     const exp = match[0];
-    //     const regexp = new RegExp(escapeRegExp(exp), "m");
-    //     const replacement = `(0${exp})`;
-    //     source = source.replace(regexp, replacement);
-    // }
 
     const g = graphviz.digraph("G");
     const addNode = makeAddNode(g);
@@ -109,90 +101,86 @@ export function createAst(text: string) {
             }
         },
 
-        IfStatement: (node, _, c) => {
-            const test = node.test as AcornNode;
-
-            const _if = addNode("if");
-            const _then = addNode("then");
-            const _else = addNode("else");
-
-            const _test = addNode(test.operator);
-
-            if (isBinaryOrUnaryExpression(test.left)) {
-                stack.push(_test);
-                c(test.left, _);
-            } else if (isLiteral(test.left)) {
-                const _left = addNode(test.left.name);
-                g.addEdge(_test, _left);
+        ExpressionStatement: (node, _, c) => {
+            if (isExpressionStatement(node)) {
+                c(node.expression, _);
+            } else {
+                c(node, _);
             }
+        },
 
-            if (isBinaryOrUnaryExpression(test.right)) {
+        IfStatement: (node, _, c) => {
+            const _if = addNode("if");
+            const _test = addNode("test");
+
+            if (isBinaryOrUnaryExpression(node.test)) {
                 stack.push(_test);
-                c(test.right, _);
-            } else if (isLiteral(test.right)) {
-                const _right = addNode(test.right.raw);
-                g.addEdge(_test, _right);
+                c(node.test, _);
             }
 
             g.addEdge(_if, _test);
-            g.addEdge(_if, _then);
-            g.addEdge(_if, _else);
 
-            {
+            if (node.consequent != null) {
+                const _then = addNode("then");
+                g.addEdge(_if, _then);
+
                 const body = node.consequent.body as AcornNode[];
-                const expressions = map(body, "expression") as AcornNode[];
-
-                expressions.forEach(node => {
+                body.forEach(node => {
                     stack.push(_then);
                     c(node, _);
                 });
             }
 
-            {
-                const body = node.alternate.body as AcornNode[];
-                const expressions = map(body, "expression") as AcornNode[];
+            if (node.alternate != null) {
+                const _else = addNode("else");
+                g.addEdge(_if, _else);
 
-                expressions.forEach(node => {
+                const body = node.alternate.body as AcornNode[];
+                body.forEach(node => {
                     stack.push(_else);
                     c(node, _);
                 });
             }
+
+            let _root: GrapNode | undefined;
+            if ((_root = stack.pop())) {
+                g.addEdge(_root, _if);
+            }
         },
 
         WhileStatement: (node, _, c) => {
-            const test = node.test as AcornNode;
-
             const _while = addNode("while");
-            const _do = addNode("do");
+            const _test = addNode("test");
 
-            const _test = addNode(test.operator);
-
-            if (isBinaryOrUnaryExpression(test.left)) {
+            if (isBinaryOrUnaryExpression(node.test)) {
                 stack.push(_test);
-                c(test.left, _);
-            } else if (isLiteral(test.left)) {
-                const _left = addNode(test.left.name);
-                g.addEdge(_test, _left);
-            }
-
-            if (isBinaryOrUnaryExpression(test.right)) {
-                stack.push(_test);
-                c(test.right, _);
-            } else if (isLiteral(test.right)) {
-                const _right = addNode(test.right.raw);
-                g.addEdge(_test, _right);
+                c(node.test, _);
             }
 
             g.addEdge(_while, _test);
+
+            const _do = addNode("do");
             g.addEdge(_while, _do);
 
             const body = node.body.body as AcornNode[];
-            const expressions = map(body, "expression") as AcornNode[];
+            const expressions = map(body, node => {
+                if (isExpressionStatement(node)) {
+                    return node.expression;
+                }
+                if (isWhileStatement(node) || isIfStatement(node)) {
+                    return node;
+                }
+            }) as AcornNode[];
 
             expressions.forEach(node => {
                 stack.push(_do);
                 c(node, _);
             });
+
+            let _root: GrapNode | undefined;
+            if ((_root = stack.pop())) {
+                g.addEdge(_root, _while);
+            }
         },
     } as TreeFunctions);
 
@@ -218,4 +206,16 @@ function isBinaryOrUnaryExpression(node: AcornNode): boolean {
 
 function isIdentifier(node: AcornNode): boolean {
     return node.type === "Identifier";
+}
+
+function isExpressionStatement(node: AcornNode): boolean {
+    return node.type === "ExpressionStatement";
+}
+
+function isWhileStatement(node: AcornNode): boolean {
+    return node.type === "WhileStatement";
+}
+
+function isIfStatement(node: AcornNode): boolean {
+    return node.type === "IfStatement";
 }
